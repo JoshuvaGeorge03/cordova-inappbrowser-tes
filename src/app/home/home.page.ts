f7f737d85bbb881fbd9d92c7b32e5f86f0729981
joshuva george <joshuva@adaptivemedia.in>
2020-01-10 17:22:09 +0530

    updated

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { InAppBrowser } from '@ionic-native/in-app-browser/ngx';
+import { Plugins } from '@capacitor/core';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +9,21 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
-
+  constructor(
+    private iab: InAppBrowser
+  ) { }
+  openUrl() {
+    const browserRef = this.iab.create('https://ionicframework.com/docs/native/in-app-browser', '_blank', {
+      location: 'no',
+      footer: 'no',
+      clearcache: 'yes',
+      clearsessioncache: 'yes',
+      toolbar: 'no',
+    });
+    browserRef.show();
+  }
+  open() {
+    const { Browser} = Plugins;
+    Browser.open({url: 'https://ionicframework.com/docs/native/in-app-browser'});
+  }
 }
